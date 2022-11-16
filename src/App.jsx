59e32b9017767a20617461646fdf99c5121c46b2
Yaya <yayazdiallo@outlook.fr>
2022-11-16 10:09:46 +0100

    fix: remove signin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/index';
-import SignInPage from './pages/signIn';
 import Error from './pages/error';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/signIn' element={<SignInPage />} />
       <Route path='*' element={<Error />} />
     </Routes>
   );
